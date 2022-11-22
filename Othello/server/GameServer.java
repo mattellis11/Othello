a71160f201115fa9e7f104ee448539cdbfb3541e
Matt Ellis <mattellis11@gmail.com>
2022-11-22 09:59:57 -0600

    added to handleMessageFromClient, added getter/setter
    implemented how to handle receipt of GameLobbyData
    implemented how to handle receipt of GameData
    added getClient()
    added setGameMaster()

--- Othello/server/GameServer.java
+++ Othello/server/GameServer.java
@@ -9,17 +9,20 @@ import javax.swing.JLabel;
 import javax.swing.JTextArea;
 
 import client.CreateAccountData;
+import client.GameLobbyData;
 import client.LoginData;
 import ocsf.server.AbstractServer;
 import ocsf.server.ConnectionToClient;
 
 public class GameServer extends AbstractServer
-{
-	// Data fields for this chat server.
+{	
 	private JTextArea log;
 	private JLabel status;
 	private boolean running = false;
 	private Database database;
+	private GameMaster gameMaster;
+	private ArrayList<String> onlinePlayers;
+	private ArrayList<Player> waitingList;
 
 	public GameServer()
 	{
@@ -101,6 +104,117 @@ public class GameServer extends AbstractServer
         return;
       }			
 		}
+		// GameLobbyData is received when a player starts a new game or a player joins a game.
+		else if (arg0 instanceof GameLobbyData)
+		{
+			GameLobbyData data = (GameLobbyData) arg0;
+			
+			// Case: A client has pressed 'Start Game'.
+			if (data.getPlayer2() == null)
+			{
+				// Store sender's client id in player1 object.
+				data.getPlayer1().setClientID(arg1.getId());
+				
+				// Add client to waitingList
+				waitingList.add(data.getPlayer1());
+				
+				// Send waiting list to all clients.
+				
+			}
+			// Case: A client has pressed 'Join Game'.
+			else if (data.getPlayer2() != null)
+			{
+				// Create a new game.
+				GameData newGame = gameMaster.newGame();
+				
+				// Set the players
+				newGame.setPlayer1(data.getPlayer1());
+				newGame.setPlayer2(data.getPlayer2());
+				
+				// Player1 will play black. Player2 will play white.
+				newGame.getPlayer1().setColor(1);
+				newGame.getPlayer2().setColor(0);
+				
+				// By default, GameData activePlayer data field is set to false.
+				// First send GameData to player2.
+				ConnectionToClient player2 = getClient(newGame.getPlayer2().getClientID());
+				try
+				{
+					player2.sendToClient(newGame);
+				} catch (IOException e)
+				{
+					e.printStackTrace();
+				}
+				
+				// Prepare GameData for player1.
+				newGame.setActivePlayer(true);
+				int[][] availableMoves = gameMaster.availableMoves(newGame.getBoardState(), newGame.getPlayer1().getColor());
+				newGame.setValidMoves(availableMoves);
+				
+				// Send GameData to player1.
+				ConnectionToClient player1 = getClient(newGame.getPlayer1().getClientID());
+				try
+				{
+					player1.sendToClient(newGame);
+				} catch (IOException e)
+				{					
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		// GameData is received when a player places a new piece on the board.		 
+		else if (arg0 instanceof GameData)
+		{
+			GameData data = (GameData) arg0;
+			
+			// Get player that sent the move.
+			Player player = data.getPlayer1().getClientID() == arg1.getId() ? data.getPlayer1() : data.getPlayer2();
+						
+			// Process new move.
+			gameMaster.placePiece(data, player.getColor());
+			
+			// Set active player to false, and return GameData to sender.
+			data.setActivePlayer(false);
+			try
+			{
+				arg1.sendToClient(data);
+			} catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+			
+			// Set active player to true, and send GameData to opponent.
+			data.setActivePlayer(true);
+			
+			// Get opponents clientId and ConnectToClient object
+			long clientId = data.getOpponent(arg1.getId());
+			ConnectionToClient opponent = getClient(clientId);
+			
+			// Send GameData to opponent.
+			try
+			{
+				opponent.sendToClient(data);
+			} catch (IOException e)
+			{				
+				e.printStackTrace();
+			}			
+		
+			
+		}
+			
+	}
+	
+	// Returns ConnectionToClient object given the clientId.
+	private ConnectionToClient getClient(long clientId)
+	{
+		ConnectionToClient[] clients = (ConnectionToClient[]) getClientConnections();
+		for (ConnectionToClient client: clients)
+		{
+			if (client.getId() == clientId) return client;
+				
+		}
+		return null;
 	}
 
 	// When the server starts, update the GUI.
@@ -165,6 +279,11 @@ public class GameServer extends AbstractServer
 	public void setDatabase(Database database)
 	{
 		this.database = database;
+	}
+
+	public void setGameMaster(GameMaster gameMaster)
+	{
+		this.gameMaster = gameMaster;
 	}
 
 }
